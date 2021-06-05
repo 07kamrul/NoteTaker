02dfe62a74bd1b9b0da7d5de3709a4df56b42241
Md Kamrul Hasan <mdkamrulhasanewu@gmail.com>
2021-06-05 16:55:41 +0600

    Note taker in hibernate

---
+++ src/main/java/com/helper/FactoryProvider.java
@@ -0,0 +1,14 @@
+package com.helper;
+
+import org.hibernate.SessionFactory;
+
+public class FactoryProvider {
+	public static SessionFactory factory;
+
+	public SessionFactory getFactory() {
+		
+		
+		return factory;
+	}
+
+}
